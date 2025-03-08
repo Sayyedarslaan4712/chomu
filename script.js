20ce03a612281ce076f19869140f5ee0c9c8707d
SayyedArslaan <166531908+Sayyedarslaan4712@users.noreply.github.com>
2025-03-08 20:14:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -182,7 +182,7 @@ const songs = [
         title: "india love",
         artist: "SayyedArslaan",
         cover: "./assets/covers/couple-2.jpg",
-        file: "./assets/songs/india-love.mp3",
+        file: "./assets/songs/india-Love.mp3",
         duration: "00:30"
     },
     {
